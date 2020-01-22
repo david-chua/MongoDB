3caaaecc45949f95a8e45bf4840fbcafd7a1a2b9
david-chua <dp_chua@hotmail.com>
2020-01-22 15:31:09 -0500

    error handling

--- Node_Bootcamp/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ Node_Bootcamp/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -61,7 +61,24 @@ const fs = require('fs');
 //   });
 // });
 
+// Error handling:
+fs.readFile('./txt/starest.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('Error found');
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+          console.log('files has been written');
+      })
+    });
+  });
+});
+
+// Working Code
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('Error found');
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
