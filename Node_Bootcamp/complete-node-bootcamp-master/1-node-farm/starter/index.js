bb67a97eeacd186ade768f34868917eba28e8619
David Chua <dp_chua@hotmail.com>
2020-01-23 09:10:43 -0500

    template

--- Node_Bootcamp/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ Node_Bootcamp/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,12 +1,35 @@
 const fs = require('fs');
 const http = require('http');
-
+const url = require('url');
 // Server:
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req,res) => {
-  res.end('Hello from the server by David!');
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview'){
+    res.end('This is the Overview');
+  } else if (pathName === '/product'){
+    res.end('This is the PRODUCT');
+  } else if (pathName ==='/api'){
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      //const productData = JSON.parse(data); --> This is moved to the top
+      //console.log(productData);
+      res.writeHead(200, {'Content-type': 'application/json'})
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
-})
+});
+// We'll use express for bigger applicaiton.
