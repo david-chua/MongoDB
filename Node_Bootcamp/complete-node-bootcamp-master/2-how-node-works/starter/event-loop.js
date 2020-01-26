c6fd7fff14709eaeb4ed706d9bf095fab16a9503
David Chua <dp_chua@hotmail.com>
2020-01-25 23:32:06 -0500

    event loop 2

--- Node_Bootcamp/complete-node-bootcamp-master/2-how-node-works/starter/event-loop.js
+++ Node_Bootcamp/complete-node-bootcamp-master/2-how-node-works/starter/event-loop.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+
+process.env.UV_THREADPOOL_SIZE = 3;
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
@@ -11,6 +16,19 @@ fs.readFile("text-file.txt", () => {
   setImmediate(() => console.log("Immediate 2 finished"))
 
   process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2('password', 'sal', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password ecrypted");
+  })
+  crypto.pbkdf2('password', 'sal', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password ecrypted");
+  })
+  crypto.pbkdf2('password', 'sal', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password ecrypted");
+  })
+  crypto.pbkdf2('password', 'sal', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password ecrypted");
+  })
 })
 
 console.log("hello from the top level code");
