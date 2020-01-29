d2dc7d5737bfbe936e784f5f337cf3e6fe4eeece
David Chua <dp_chua@hotmail.com>
2020-01-29 00:50:02 -0500

    starting post

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 // app.get('/', (req,res) => {
 //     res.status(200).json({
 //       message: 'Hello from the server side!',
@@ -20,11 +21,16 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req,res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours
     }
   });
 });
+
+app.post('/api/v1/tours', (req,res) => {
+
+})
 
 const port = 3000;
 
