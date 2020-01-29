5bba430629ffb4debcfa33d9cd4964311a5119d1
David Chua <dp_chua@hotmail.com>
2020-01-29 00:23:49 -0500

    tours starting

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,16 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.status(200).json({
-      message: 'Hello from the server side!',
-      app: 'NA Tours'});
-})
+// app.get('/', (req,res) => {
+//     res.status(200).json({
+//       message: 'Hello from the server side!',
+//       app: 'NA Tours'});
+// })
+//
+// app.post('/', (req,res) =>{
+//   res.send('You can post to this URL endpoint...');
+// })
 
-app.post('/', (req,res) =>{
-  res.send('You can post to this URL endpoint...');
-})
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req,res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours
+    }
+  });
+});
 
 const port = 3000;
 
