635ae33288b0eeed5971230d0fd9d6b476d8f7bd
david-chua <dp_chua@hotmail.com>
2020-02-05 12:33:56 -0500

    node variables

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -5,10 +5,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
+// console.log(process.env.NODE_ENV);
 // Middelware
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next) => {
   console.log('Hello from the middleware');
