c527df18169c19e9eaf74aafc9351bc30fca3976
David Chua <dp_chua@hotmail.com>
2020-02-06 23:54:15 -0500

    learning to sort

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -29,10 +29,24 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req,res) => {
   try {
+    // 1. Filtering
     const queryObj = {...req.query}
     const excludeFields = ['page', 'sort', 'limit', 'fields']
     excludeFields.forEach(el => delete queryObj[el]);
-    const query = Tour.find(queryObj)
+
+    // Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort){
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
