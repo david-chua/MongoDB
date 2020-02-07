27d4cc396f535a437d78e5c4163f5fbbe28abfab
david-chua <dp_chua@hotmail.com>
2020-02-07 13:37:35 -0500

    page and limiting results

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -55,6 +55,18 @@ exports.getAllTours = async (req,res) => {
       query = query.select('-__v');
     }
 
+    // pagination
+    // page=2 and limit=10
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page -1) * limit
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page){
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
