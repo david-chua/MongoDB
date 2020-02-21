c926ce103b37de076f1c2d692e734812946f1186
David Chua <dp_chua@hotmail.com>
2020-02-20 23:00:56 -0500

    creating 404 error

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -2,6 +2,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/APIFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -57,6 +58,10 @@ exports.getAllTours = catchAsync(async (req,res, next) => {
 exports.getTour = catchAsync(async(req,res,next) => {
   const tour = await Tour.findById(req.params.id);
   //const tour = await Tour.findOne({ __id: req.params.id});
+  if (!tour){
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -83,6 +88,10 @@ exports.updateTour = catchAsync(async (req,res,next) => {
     runValidators: true
   })
 
+  if (!tour){
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -92,7 +101,11 @@ exports.updateTour = catchAsync(async (req,res,next) => {
 });
 
 exports.deleteTour = catchAsync(async (req,res,next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour){
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
