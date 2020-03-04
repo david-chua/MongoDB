8c43c37a8f923db202f8a71fccf1e71065cd4a85
David Chua <dp_chua@hotmail.com>
2020-03-03 21:32:56 -0500

    user schema

---
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name']
+  },
+  email:{
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password:{
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password cannot be shorter than 8 characters']
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    minlength: [8, 'Pease confirm your password']
+  }
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
