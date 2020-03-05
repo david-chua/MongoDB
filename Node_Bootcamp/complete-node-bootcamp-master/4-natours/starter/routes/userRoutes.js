ef8d8f434947fbcacae4b6fa90b1ddcb777a2dcb
David Chua <dp_chua@hotmail.com>
2020-03-04 23:25:58 -0500

    writing routes

--- Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/routes/userRoutes.js
+++ Node_Bootcamp/complete-node-bootcamp-master/4-natours/starter/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const {getAllUsers, createUser, getUser, updateUser, deleteUser} = require('./../controllers/userController');
+const {signup} = require('./../controllers/authController');
 const router = express.Router();
 
+router.post('/signup', signup);
 
 router
   .route('/')
